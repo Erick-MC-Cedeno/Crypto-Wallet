c2288ca9d4bf8dd10d8ad2a1835a3fbf8df50185
Cisco101010 <erickcedeno.ec@gmail.com>
2024-10-09 21:46:14 -0600

    New Updates

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,88 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import { Grid, Paper, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom'; // Asegúrate de importar Link
+import { Link } from 'react-router-dom';
 import TotalBalance from './TotalBalance';
 import MyWallets from './MyWallets';
 
 const Dashboard = () => {
-    // Estado para el texto que se va a mostrar con efecto de escritura
     const [displayText, setDisplayText] = useState('');
-    const fullText = "La seguridad de tu cuenta es nuestra prioridad. Asegúrate de habilitar la autenticación de dos factores para proteger tus activos.";
+    const texts = [
+        "La seguridad de tu cuenta es nuestra prioridad. Asegúrate de habilitar la autenticación de dos factores para proteger tus activos.",
+        "Con las criptomonedas, puedes ahorrar y obtener más rendimientos. ¡Descubre el potencial de tu dinero!",
+        "Utiliza nuestro servicio P2P para vender tus tokens por fiat de manera rápida y segura.",
+        "La blockchain es una tecnología revolucionaria que permite transacciones seguras y transparentes. Aprende cómo aprovecharla para tus inversiones."
+    ];
+    const [textIndex, setTextIndex] = useState(0);
 
     useEffect(() => {
         let index = 0;
-
         const interval = setInterval(() => {
-            if (index < fullText.length) {
-                setDisplayText((prev) => prev + fullText.charAt(index));
+            if (index < texts[textIndex].length) {
+                setDisplayText(prev => prev + texts[textIndex].charAt(index));
                 index++;
             } else {
+                setTimeout(() => {
+                    setTextIndex(prev => (prev + 1) % texts.length);
+                    setDisplayText('');
+                    index = 0;
+                }, 2000);
                 clearInterval(interval);
             }
-        }, 50); // Cambia la velocidad del efecto de escritura ajustando el valor
+        }, 50);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [textIndex]);
 
     return (
-        <Grid container spacing={3} sx={{ padding: 2 }}> 
+        <Grid container spacing={3} sx={{ padding: 2 }}>
             <Grid item xs={12}>
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={5} lg={4}>
-                        <Paper
-                            sx={{
-                                p: 3, 
-                                display: 'flex',
-                                flexDirection: 'column',
-                                height: 300,
-                                borderRadius: 2,
-                                boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-                                border: '1px solid #e0e0e0',
-                                bgcolor: '#ffffff',
-                            }}
-                        >
-                            <Typography variant="h6" sx={{ mb: 2 }}>
-                                Cuenta
-                            </Typography>
+                        <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 300, borderRadius: 2, boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', border: '1px solid #e0e0e0', bgcolor: '#ffffff' }}>
+                            <Typography variant="h6" sx={{ mb: 2 }}>Cuenta</Typography>
                             <TotalBalance />
                         </Paper>
                     </Grid>
 
                     <Grid item xs={12} md={7} lg={8}>
-                        <Paper
-                            sx={{
-                                p: 3,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                height: 300,
-                                borderRadius: 2,
-                                boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-                                border: '1px solid #e0e0e0',
-                                bgcolor: '#ffffff',
-                            }}
-                        >
-                            <Typography variant="h6" sx={{ mb: 2 }}>
-                                Resumen de Actividades
-                            </Typography>
-                            <Typography variant="body1" sx={{ mb: 2 }}>
-                                {displayText} {/* Mostrar el texto con efecto de escritura */}
+                        <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 300, borderRadius: 2, boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', border: '1px solid #e0e0e0', bgcolor: '#ffffff' }}>
+                            <Typography variant="h6" sx={{ mb: 2, color: 'blue' }}>Bienvenido Crypto entusiasta</Typography>
+                            <Typography variant="body1" sx={{ mb: 2, fontSize: '1.2rem', fontWeight: 'bold', color: '#333', transition: 'color 0.3s ease', '&:hover': { color: '#007bff' } }}>
+                                {displayText}
                             </Typography>
                             <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: 'auto' }}>
-                                <Link to="/wallets" style={{ textDecoration: 'none' }}>
-                                    <Button variant="contained" color="primary" sx={{ mr: 1, bgcolor: 'blue', '&:hover': { bgcolor: 'darkblue' } }}>
-                                        Depositar
-                                    </Button>
-                                </Link>
-                                <Link to="/wallets" style={{ textDecoration: 'none' }}>
-                                    <Button variant="contained" color="primary" sx={{ mr: 1, bgcolor: 'blue', '&:hover': { bgcolor: 'darkblue' } }}>
-                                        Retirar
-                                    </Button>
-                                </Link>
-                                <Link to="/settings" style={{ textDecoration: 'none' }}>
-                                    <Button variant="contained" color="primary" sx={{ bgcolor: 'blue', '&:hover': { bgcolor: 'darkblue' } }}>
-                                        2FA auth
-                                    </Button>
-                                </Link>
+                                {['/wallets', '/wallets', '/settings'].map((link, index) => (
+                                    <Link key={index} to={link} style={{ textDecoration: 'none' }}>
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            sx={{
+                                                mr: 1,
+                                                bgcolor: 'blue',
+                                                borderRadius: '12px', // Ajustar el borde redondeado aquí
+                                                '&:hover': { bgcolor: 'darkblue' },
+                                            }}
+                                        >
+                                            {index === 0 ? 'Depositar' : index === 1 ? 'Retirar' : '2FA auth'}
+                                        </Button>
+                                    </Link>
+                                ))}
                             </div>
                         </Paper>
                     </Grid>
@@ -90,20 +74,8 @@ const Dashboard = () => {
             </Grid>
 
             <Grid item xs={12}>
-                <Paper
-                    sx={{
-                        p: 3, 
-                        display: 'flex',
-                        flexDirection: 'column',
-                        borderRadius: 2,
-                        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-                        border: '1px solid #e0e0e0',
-                        bgcolor: '#ffffff',
-                    }}
-                >
-                    <Typography variant="h6" sx={{ mb: 2 }}>
-                        Mis Wallets
-                    </Typography>
+                <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', borderRadius: 2, boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', border: '1px solid #e0e0e0', bgcolor: '#ffffff' }}>
+                    <Typography variant="h6" sx={{ mb: 2 }}>Mis Wallets</Typography>
                     <MyWallets />
                 </Paper>
             </Grid>
