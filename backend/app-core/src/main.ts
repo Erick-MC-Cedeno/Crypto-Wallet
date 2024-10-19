779c0ff73587de332796d56d844dafadf1e1208c
Cisco101010 <erickcedeno.ec@gmail.com>
2024-10-18 19:45:31 -0600

    New Updates  Tonight

--- backend/app-core/src/main.ts
+++ backend/app-core/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://192.168.100.2:3000'],
     credentials: true
   })
 
