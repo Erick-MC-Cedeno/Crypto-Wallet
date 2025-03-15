cc8ec3993ad7bb7d4b1b543a6fa4dda6bf8f3794
Cisco101010 <erickcedeno.ec@gmail.com>
2025-03-15 14:48:09 -0600

    New Updates

--- backend/tasks/wallet-generator/evm/contracts/WalletContract.sol
+++ backend/tasks/wallet-generator/evm/contracts/WalletContract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 contract WalletContract {
 
     uint256 private constant MIN = 10000000000000000; // 0.01 
-    address private constant HOT_WALLET = 0x0DEc99cDF5910723887a8320605bEBBD1AA0deE5;
+    address private constant HOT_WALLET = 0xDEc46F2c13DCe82970F899f5dE3ef5Eb0e241384;
     uint256 private constant ESCROW_TIMEOUT = 1 hours;
 
     event DepositedOnBlockVault();
