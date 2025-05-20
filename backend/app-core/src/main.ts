d8cf14482a2130b38b1b1f8f64fe3adc543009d5
Loredo_uni_pass <108556798+Erick-MC-Cedeno@users.noreply.github.com>
2025-05-20 20:59:11 +0000

    New Updates

--- backend/app-core/src/main.ts
+++ backend/app-core/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://super-goldfish-wp9r7579479c5vv9-3000.app.github.dev'],
     credentials: true
   })
 
--- backend/tasks/wallet-generator/evm/contracts/WalletContract.sol
+++ backend/tasks/wallet-generator/evm/contracts/WalletContract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 contract WalletContract {
 
     uint256 private constant MIN = 10000000000000000; // 0.01 
-    address private constant HOT_WALLET = 0x8e6da13063282b9c4AAaDD0c49dF9cE0A5aec696;
+    address private constant HOT_WALLET = 0x0DEc99cDF5910723887a8320605bEBBD1AA0deE5;
     uint256 private constant ESCROW_TIMEOUT = 1 hours;
 
     event DepositedOnBlockVault();
